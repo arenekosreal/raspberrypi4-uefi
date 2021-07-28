7f9e2258d3bbb47996d2084e807b0c70bcd17ec7
zhanghua000 <zhanghua.00@qq.com>
2021-07-28 16:38:46 +0800

    Fix write privillege on GitHub CI environment

--- start-build.sh
+++ start-build.sh
@@ -2,4 +2,7 @@
 if [[ ! -f PKGBUILD ]];then
     git clone https://github.com/zhanghua000/raspberrypi-uefi-boot .
 fi
+if [[ ${CI} == true ]];then
+    sudo chown -R builder:builder .
+fi
  makepkg -fd --config=/home/builder/makepkg-aarch64.conf
