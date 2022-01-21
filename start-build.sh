942f275047e2d8dd79c0ec0a54c4fa61b9798b87
zhanghua000 <zhanghua.00@qq.com>
2022-01-21 17:13:11 +0800

    Split into several packages

--- start-build.sh
+++ start-build.sh
@@ -1,8 +1,16 @@
 #! /usr/bin/env bash
-if [[ ! -f PKGBUILD ]];then
-    git clone https://github.com/zhanghua000/raspberrypi-uefi-boot .
-fi
+
 if [[ ${CI} == true ]];then
     sudo chown -R builder:builder .
 fi
- makepkg -fd --config=/home/builder/makepkg-aarch64.conf
+root=$PWD
+mkdir -p out
+for package in raspberrypi4-uefi-firmware-git raspberrypi4-uefi-kernel-generic-git raspberrypi4-uefi-kernel-raspberrypi-git
+do
+    cd ${root}/${package}
+    makepkg -fd --config=/home/builder/makepkg-aarch64.conf
+    cp *.pkg.tar.zst ${root}/out
+done
+cd ${root}/out
+
+
