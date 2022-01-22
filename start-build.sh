abe4b49c6c446e6de8b25219f8697e108795587e
zhanghua000 <zhanghua.00@qq.com>
2022-01-22 17:57:02 +0800

    Enable compressed firmware in generic kernel

--- start-build.sh
+++ start-build.sh
@@ -8,7 +8,9 @@ else
     root=$PWD
     conf=${root}/makepkg-aarch64.conf
 fi
+echo "\$root is ${root}."
 mkdir -p ${root}/out
+rm -f ${root}/out/*.pkg.tar.zst
 for package in $(find ${root} -type f -name PKGBUILD | sed "s_${root}/__;s_/PKGBUILD__")
 do
     echo "Processing ${package} folder..."
